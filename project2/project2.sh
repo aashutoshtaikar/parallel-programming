55e9d735018865322a540f9b2c6a41725b03997a
AashutoshYeshwa Taikar <taikara@nome00.eecs.oregonstate.edu>
2017-05-13 01:30:23 -0700

    updated project2

--- project2/project2.sh
+++ project2/project2.sh
@@ -5,7 +5,7 @@ for x in 1 4096
 do
         for y in 1 2 4 8 10 12 16 32
         do
-        g++ -DNUMT=$y -Dchunksize=$x project2_0.cpp -o project2 -lm -fopenmp
+        g++ -DNUMT=$y -Dchunksize=$x project2.cpp -o project2 -lm -fopenmp
         ./project2
         done
 done
